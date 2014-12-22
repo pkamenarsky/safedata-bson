bb50ec0844f00d86ec0dc716a88fc0163d20f731
Philip Kamenarsky <p.kamenarsky@gmail.com>
2014-12-22 01:29:53 +0100

    Val instance

--- src/Data/SafeData/Bson.hs
+++ src/Data/SafeData/Bson.hs
@@ -1,44 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Data.SafeData.Bson
-  ( toValue
-  , toDocument
-  ) where
+module Data.SafeData.Bson where
 
 import qualified Data.Bson            as B
-import           Data.Bson            (Field ((:=)), (=:))
+import           Data.Bson            ((=:))
 import qualified Data.SafeData        as S
 import qualified Data.Text            as T
 import qualified Data.Vector          as V
+import           Data.Word
 
-toValue :: S.Value -> B.Value
-toValue (S.BValue v)   = B.Bool v
-toValue (S.CValue v)   = B.String $ T.singleton v
-toValue (S.SValue v)   = B.String $ T.pack v
-toValue (S.DValue v)   = B.Float v
-toValue (S.FValue v)   = B.Float $ realToFrac v
-toValue (S.IValue v)   = B.Float $ fromInteger $ toInteger v
-toValue (S.I8Value v)  = B.Float $ fromInteger $ toInteger v
-toValue (S.I16Value v) = B.Float $ fromInteger $ toInteger v
-toValue (S.I32Value v) = B.Float $ fromInteger $ toInteger v
-toValue (S.I64Value v) = B.Float $ fromInteger $ toInteger v
-toValue (S.BIValue v)  = undefined
-toValue (S.OrdValue v) = undefined
-toValue (S.WValue v)   = undefined
-toValue (S.W8Value v)  = undefined
-toValue (S.W16Value v) = undefined
-toValue (S.W32Value v) = undefined
-toValue (S.W64Value v) = undefined
-toValue (S.UValue v)   = undefined
-toValue (S.BSValue v)  = undefined
-toValue (S.BSLValue v) = undefined
+instance B.Val Word8 where
+  val                = B.Float . fromInteger . toInteger
+  cast' (B.Float v)  = Just $ fromInteger $ round v
+  cast' _            = Nothing
 
-toValue (S.Array v)    = B.Array $ map toValue v
+instance B.Val S.Value where
+  val (S.BValue v)   = B.Bool v
+  val (S.CValue v)   = B.String $ T.singleton v
+  val (S.SValue v)   = B.String $ T.pack v
+  val (S.DValue v)   = B.Float v
+  val (S.FValue v)   = B.Float $ realToFrac v
+  val (S.IValue v)   = B.Float $ fromInteger $ toInteger v
+  val (S.I8Value v)  = B.Float $ fromInteger $ toInteger v
+  val (S.I16Value v) = B.Float $ fromInteger $ toInteger v
+  val (S.I32Value v) = B.Float $ fromInteger $ toInteger v
+  val (S.I64Value v) = B.Float $ fromInteger $ toInteger v
+  val (S.BIValue v)  = undefined
+  val (S.OrdValue v) = undefined
+  val (S.WValue v)   = undefined
+  val (S.W8Value v)  = undefined
+  val (S.W16Value v) = undefined
+  val (S.W32Value v) = undefined
+  val (S.W64Value v) = undefined
+  val (S.UValue v)   = undefined
+  val (S.BSValue v)  = undefined
+  val (S.BSLValue v) = undefined
 
-toDocument (S.Object ty cstr version vs)
-                      = [ "_type"    := B.String (T.pack ty)
-                        , "_cstr"    := B.Float  (fromInteger $ toInteger cstr)
-                        , "_version" := B.Float  (fromInteger $ toInteger version)
-                        ]
-                        ++ map (\(k, v) -> T.pack k := toValue v) vs
-toDocument _ = error "Expected an Object"
+  val (S.Array v)    = B.Array $ map B.val v
+
+  val (S.Object ty cstr version vs)
+                     = B.Doc $ [ "_type"    =: ty
+                               , "_cstr"    =: cstr
+                               , "_version" =: version
+                               ]
+                               ++ map (\(k, v) -> T.pack k =: v) vs
+
+  cast' (B.Bool v) = Just $ S.BValue v
